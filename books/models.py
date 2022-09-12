1b94e2082041849f0da917ce348e57bede1ff4df
mamadreza1214 <m.ramezani6017@gmail.com>
2022-09-12 23:36:55 +0430

    create a model for books app to send sqlite3

--- books/models.py
+++ books/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.shortcuts import reverse
 
-# Create your models here.
+
+class Book(models.Model):
+    STATUS_CHOICES = (
+        ('pub', 'Published'),
+        ('drf', 'Draft'),
+    )
+    BOOK_LANGUAGE = (
+        ('lge', 'persian'),
+        ('lge', 'english'),
+    )
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    translator = models.CharField(max_length=100, null=True, blank=True)
+    publisher = models.CharField(max_length=100)
+    year_of_publication = models.IntegerField()
+    number_of_pages = models.IntegerField()
+    language = models.CharField(max_length=50, choices=BOOK_LANGUAGE, default='persian')
+
+    def __str__(self):
+        return self.title
+
